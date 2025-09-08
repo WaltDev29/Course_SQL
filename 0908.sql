2437c5bc2b2c5920abab8ecc763fe8300992805b
Waltdev29 <waltdev29@gmail.com>
2025-09-08 15:50:02 +0900

    ORDER BY

--- 0908.sql
+++ 0908.sql
@@ -40,3 +40,32 @@ WHERE 나이 IS NOT NULL;
 SELECT 고객이름, 나이
 FROM 고객
 WHERE 나이 >= 25;
+
+-- ### ORDER BY
+-- NULL이 나오는 순서 ASC : 맨 뒤, DESC : 맨 처음
+
+SELECT 고객이름, 등급, 나이
+FROM 고객
+ORDER BY 나이 DESC;
+
+SELECT 고객이름, 나이, 등급, 직업
+FROM 고객
+WHERE 등급 = 'gold' OR 등급 = 'vip'
+ORDER BY 등급 ASC;
+
+SELECT *
+FROM 주문
+WHERE (주문제품 = 'p03' OR 주문제품 = 'p06') AND 배송지 LIKE '경기도%'
+ORDER BY 수량 DESC;
+
+SELECT * FROM 주문;
+
+SELECT 주문고객, 주문제품, 수량, 주문일자
+FROM 주문
+WHERE 수량 >= 10
+ORDER BY 주문제품 ASC, 수량 DESC;
+
+SELECT 주문고객, 주문제품, 수량, 배송지
+FROM 주문
+WHERE (주문고객 LIKE 'a%' OR 주문고객 LIKE 'b%') AND 수량 >= 10
+ORDER BY 주문고객 DESC, 주문제품 DESC;
