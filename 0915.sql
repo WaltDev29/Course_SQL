71acba6420d5bc89e6e2bfe89ff3fba3d9410275
Waltdev29 <waltdev29@gmail.com>
2025-09-15 14:44:21 +0900

    GROUP BY

--- 0915.sql
+++ 0915.sql
@@ -4,4 +4,23 @@ SELECT COUNT(DISTINCT 제조업체) AS "제조업체 수" FROM 제품;
 SELECT COUNT(DISTINCT 주문고객) AS "주문고객 수" FROM 주문;
 
 -- ### GROUP BY
+SELECT * FROM 주문;
 
+SELECT 주문제품, SUM(수량) AS 총주문수량
+FROM 주문
+GROUP BY 주문제품
+ORDER BY 주문제품 ASC;
+
+SELECT 주문고객, ROUND(AVG(수량),1) AS 수량평균
+FROM 주문
+GROUP BY 주문고객
+ORDER BY 주문고객 ASC;
+
+SELECT 등급, ROUND(AVG(적립금),2)
+FROM 고객
+GROUP BY 등급
+ORDER BY 등급;
+
+SELECT 제조업체, COUNT(*) AS 제품수, MAX(단가) AS 최고가
+FROM 제품
+GROUP BY 제조업체;
