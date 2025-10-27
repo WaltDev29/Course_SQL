28bd4407bded7b451ca51baf66c9c12edd91dbcf
Waltdev29 <waltdev29@gmail.com>
2025-10-27 16:25:07 +0900

    INSERT 기능 추가

--- SQL/src/mvc_jdbc_test/controller/MainController.java
+++ SQL/src/mvc_jdbc_test/controller/MainController.java
@@ -5,20 +5,26 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import jdbc_test.JDBCConnector;
 import mvc_jdbc_test.entity.Customer;
 import mvc_jdbc_test.entity.Order;
 import mvc_jdbc_test.view.CustomerView;
+import mvc_jdbc_test.view.InputCustomerInfoView;
 import mvc_jdbc_test.view.OrderView;
 
 public class MainController {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Connection con;
+
         ArrayList<Customer> customerList;
         ArrayList<Order> orderList;
 
+        // 정보 SELECT
         try {
-            Connection con = JDBCConnector.getConnection();
+            con = JDBCConnector.getConnection();
             customerList = getCustomerList(con);
             orderList = getOrderList(con);
 
@@ -28,6 +34,11 @@ public class MainController {
         }
         printCustomerList(customerList);
         printOrderList(orderList);
+
+        // 고객 정보 INSERT
+        inputCustomerInfo(con, sc);
+
+        sc.close();
     }
 
     public static ArrayList<Customer> getCustomerList(Connection con) throws SQLException {
@@ -100,6 +111,61 @@ public class MainController {
         }
         ov.printFoot();
     }
+
+    public static void inputCustomerInfo(Connection con, Scanner sc) {
+        InputCustomerInfoView iciv = new InputCustomerInfoView();
+        ArrayList<Customer> inputCustomerList = new ArrayList<>();
+        String answer;
+
+        while (true) {
+            Customer customer = iciv.inputCustomerInfo(sc);
+
+            // 데이터 저장 여부 확인
+            while (true) {
+                System.out.println("\n데이터 저장 : S\n다시 입력 : R");
+                System.out.print("입력 : ");
+                answer = sc.nextLine();
+
+                if (answer.equalsIgnoreCase("S")) break;
+                else if (answer.equalsIgnoreCase("R")) break;
+                else System.out.println("잘못된 입력입니다. 다시 입력해주세요.\n");
+            }
+            if (answer.equalsIgnoreCase("R")) continue;
+
+            // 데이터 INSERT
+            try {
+                String sql = "INSERT INTO 고객 VALUES(?,?,?,?,?,?)";
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setString(1, customer.getId());
+                ps.setString(2, customer.getName());
+                ps.setInt(3, customer.getAge());
+                ps.setString(4, customer.getGrade());
+                ps.setString(5, customer.getJob());
+                ps.setInt(6, customer.getPoint());
+                ps.executeUpdate();
+                ps.close();
+            } catch (SQLException e) {
+                System.out.println("Statement or SQL Error");
+                throw new RuntimeException(e);
+            }
+            inputCustomerList.add(customer);
+
+            // 계속 입력받을지 선택
+            while (true) {
+                System.out.println("\n계속 입력 : C\n입력 종료 : E");
+                System.out.print("입력 : ");
+                answer = sc.nextLine();
+
+                if (answer.equalsIgnoreCase("E")) break;
+                else if (answer.equalsIgnoreCase("C")) break;
+                else System.out.println("잘못된 입력입니다. 다시 입력해주세요.\n");
+            }
+            if (answer.equalsIgnoreCase("E")) break;
+        }
+
+        // 입력 내용 출력
+        System.out.println("입력 종료");
+        System.out.println("====== 입력 내용 ======\n");
+        printCustomerList(inputCustomerList);
+    }
 }
-
-
--- SQL/src/mvc_jdbc_test/view/CustomerView.java
+++ SQL/src/mvc_jdbc_test/view/CustomerView.java
@@ -5,22 +5,33 @@ import mvc_jdbc_test.entity.Customer;
 public class CustomerView {
     public String title = "고객 정보";
 
+//    public void printCustomer(Customer customer) {
+//        System.out.printf("※ 고객명 : %s\n", customer.getName());
+//        System.out.printf("※ 나이 : %d\n", customer.getAge());
+//        System.out.printf("※ 등급 : %s\n", customer.getGrade());
+//        System.out.printf("※ 직업 : %s\n", customer.getJob());
+//        System.out.printf("※ 적립금 : %d\n", customer.getPoint());
+//    }
+
     public void printCustomer(Customer customer) {
-        System.out.printf("※ 고객명 : %s\n", customer.getName());
-        System.out.printf("※ 나이 : %d\n", customer.getAge());
-        System.out.printf("※ 등급 : %s\n", customer.getGrade());
-        System.out.printf("※ 직업 : %s\n", customer.getJob());
-        System.out.printf("※ 적립금 : %d\n", customer.getPoint());
+        System.out.printf("%-14s\t", customer.getId());
+        System.out.printf("%-8s\t", customer.getName());
+        System.out.printf("%-4d\t", customer.getAge());
+        System.out.printf("%-10s\t", customer.getGrade());
+        System.out.printf("%-10s\t", customer.getJob());
+        System.out.printf("%-8d\t", customer.getPoint());
     }
 
     public void printHead() {
-        System.out.println("========================");
+        System.out.println("\n========================");
         System.out.println("========"+title+"=======");
-        System.out.println("========================");
+        System.out.println("========================\n");
+        System.out.printf("%-10s\t%-10s%-4s\t%-10s\t%-10s\t%-8s\n", "고객아이디", "고객이름", "나이", "등급", "직업", "적립금");
+        System.out.println("-------------------------------------------------------------------------------");
     }
 
     public void printFoot() {
-        System.out.println("========================");
+        System.out.println("\n========================");
         System.out.println("====== Print Done ======");
         System.out.println("========================\n");
     }
